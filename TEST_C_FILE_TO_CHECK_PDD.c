a31bda43f03b606c7cf9bf6a3bf6cc848304f0ac
levBagryansky <28lev11@gmail.com>
2023-09-06 23:33:34 +0300

    Pdd should create @todo

--- TEST_C_FILE_TO_CHECK_PDD.c
+++ TEST_C_FILE_TO_CHECK_PDD.c
@@ -1,7 +1,7 @@
 /**
  * @todo #2238:30min Aboba Aboba AbobaAboba Aboba
  * Aboba Aboba AbobaAboba AbobaAboba Aboba AbobaAboba Aboba
- * Aboba Aboba AbobaAboba AbobaAboba Aboba AbobaAboba Aboba
+ * Aboba Aboba AbobaAboba AbobaAboba Aboba  AbobaAboba Aboba
  * @return 0.
  */
 
