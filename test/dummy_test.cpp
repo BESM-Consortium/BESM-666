e6945811f677f99d2ab115ec98a62e458a8be1c6
mmamayka <mr.green4000@gmail.com>
2023-09-07 23:23:56 +0300

    Unit testing infra

---
+++ test/dummy_test.cpp
@@ -0,0 +1,13 @@
+
+#include <gtest/gtest.h>
+
+// Try it youself, CI won't allow me to merge this
+/*
+TEST(dummy, dummy_test_fail) {
+    EXPECT_TRUE(false);
+}
+*/
+
+TEST(dummy, dummy_test_success) {
+    EXPECT_TRUE(true);
+}
