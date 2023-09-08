1cee7372a3497dcfcb5f2ed50b68663a6151d36b
mmamayka <mr.green4000@gmail.com>
2023-09-08 12:53:28 +0300

    Fix clang format tool '-or' placement

--- tools/clang-format.sh
+++ tools/clang-format.sh
@@ -2,7 +2,7 @@ if [ ! -d "$1" ]; then
     exit 0
 fi
 
-find $1 -name '*.cpp' -or -name '*.hpp' \
-         -name '*.hxx' -or -name '*.cxx' \
+find $1 -name '*.cpp' -or -name '*.hpp' -or \
+         -name '*.hxx' -or -name '*.cxx' -or \
          -name '*.h'  | \
     xargs -n 1 clang-format -i
