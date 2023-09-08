0fcf0286d9262012eefb30e9c947f928b82475b1
mmamayka <mr.green4000@gmail.com>
2023-09-08 12:43:53 +0300

    Fix quotation in clang-format tool

--- tools/clang-format.sh
+++ tools/clang-format.sh
@@ -2,7 +2,7 @@ if [ ! -d "$1" ]; then
     exit 0
 fi
 
-find $1 -name "*.cpp" -or -name "*.hpp" \
-         -name "*.hxx" -or -name "*.cxx" \
-         -name "*.h"  | \
+find $1 -name '*.cpp' -or -name '*.hpp' \
+         -name '*.hxx' -or -name '*.cxx' \
+         -name '*.h'  | \
     xargs -n 1 clang-format -i
