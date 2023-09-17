0007f6f15dcfe4287e2b3c31c3ca434e2e895987
Lev Bagryansky <32939651+levBagryansky@users.noreply.github.com>
2023-09-17 18:32:10 +0300

    Introduced instruction commands enum (#34)
    Introduced Instruction commands enum of rv64i and edited Instruction struct

--- include/besm-666/instruction.hpp
+++ include/besm-666/instruction.hpp
@@ -1,28 +1,20 @@
 #pragma once
 
 #include "besm-666/riscv-types.hpp"
+#include "besm-666/rv-instruction-op.hpp"
 
 namespace besm {
 
 using Register = uint8_t;
 using Opcode = uint8_t;
-using Immidiate = uint64_t;
-
-enum class OperandType { UNUSED, REGISTER, IMMIDIATE };
-
-struct Operand {
-    OperandType type;
-    union {
-        Register reg;
-        Immidiate imm;
-    };
-};
+using Immidiate = uint32_t;
 
 struct Instruction {
-    Opcode opcode;
-    Operand op1;
-    Operand op2;
-    Operand op3;
+    const Register rd;
+    const Register rs1;
+    const Register rs2;
+    const Immidiate immidiate;
+    const InstructionOp operation;
 };
 
 } // namespace besm
