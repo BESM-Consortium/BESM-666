a848024df33a9b91fc5e406bf7c0de41fdd7bbb2
mmamayka <mr.green4000@gmail.com>
2023-09-19 20:39:32 +0300

    Use RV64UDWord for immidiate value

--- include/besm-666/instruction.hpp
+++ include/besm-666/instruction.hpp
@@ -7,13 +7,13 @@ namespace besm {
 
 using Opcode = uint8_t;
 using Register = uint8_t;
-using Immidiate = uint32_t;
+using Immidiate = RV64UDWord;
 
 struct Instruction {
     Register rd;
     Register rs1;
     Register rs2;
-    Immidiate immidiate;
+    RV64UDWord immidiate;
     InstructionOp operation;
 };
 
